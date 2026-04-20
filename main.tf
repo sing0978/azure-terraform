f70458dc49fbb28c355ea6c6ff3cfa420230af8d
Gurjot Singh <gurjotsingh@gsingh2-FKWFWX4XXF.local>
2026-04-20 17:58:56 -0400

    Azure Terraform

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "guacamolerg"
+  location = "westus2"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "guacamolevnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_subnet" "guacsubnet" {
+  name                 = "guacsubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
---
+++ vm.tf
@@ -0,0 +1,61 @@
+resource "azurerm_network_interface" "nic" {
+  name                = "rg-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.guacsubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pubip.id
+  }
+}
+
+resource "azurerm_public_ip" "pubip" {
+  name                = "acceptanceTestPublicIp1"
+  sku		      = "Standard"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "rg" {
+  name                = "rg-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "guacadmin"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+  custom_data =  base64encode(<<-EOF
+	#!/bin/bash
+	sudo apt update -y 
+	sudo apt upgrade -y
+	sudo apt install xrdp -y
+	sudo systemctl enable xrdp
+	sudo systemctl start xrdp
+   EOF
+   )
+
+  admin_ssh_key {
+    username   = "guacadmin"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}

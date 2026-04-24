5b2da3202a861e9e5b87b5618b574d57af9f58a5
Gurjot Singh <38872085+sing0978@users.noreply.github.com>
2026-04-23 21:51:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "rg" {
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "guacamolevnet"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["<network ip>/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
@@ -31,5 +31,5 @@ resource "azurerm_subnet" "guacsubnet" {
   name                 = "guacsubnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["<netowork ip>/24"]
 }
